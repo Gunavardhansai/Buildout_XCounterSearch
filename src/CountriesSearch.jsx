d0ea0f765bef49acb78f825dc50b779560eac1f8
GUNAVARDHANSAIPUTTA <gunavardhansaiputta@gmail.com>
2025-04-03 12:58:12 +0530

    Test commit

--- src/CountriesSearch.jsx
+++ src/CountriesSearch.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const CountriesSearch = ({ data }) => {
   return (
     <div className="countryCard">
-      {data.flags && data.flags.svg ? (
-        <img src={data.flags.svg} alt={data.name.common} />
+      {data.common && data.png ? (
+        <img src={data.png} alt={data.common} />
       ) : (
         <p>No Flag Available</p>
       )}
-      <h2>{data.name?.common || "Unknown Country"}</h2>
+      <h2>{data?.common }</h2>
     </div>
   );
 };
